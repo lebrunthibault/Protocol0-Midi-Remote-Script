afa988fae2fab64bf40ea57b4fb5f768bc0ab4f4
Thibault <thibaultlebrun@live.fr>
2021-11-12 20:17:00 +0100

    'backup'

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
+import os
 import sys
+from os.path import dirname
 
 from protocol0_midi.Protocol0Midi import Protocol0Midi
 
-sys.path.insert(0, "C:\\Python27\\Lib\\site-packages")
+sys.path.insert(0, "%s\\protocol0\\venv\\Lib\\site-packages" % dirname(dirname(os.path.realpath(__file__))))
 
 def create_instance(c_instance):  # noqa
     return Protocol0Midi(c_instance)
