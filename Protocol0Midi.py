408f0e07a242e1d7fe309a3dae1975055476c6b8
Thibault <thibaultlebrun@live.fr>
2022-02-07 01:00:56 +0100

    'backup'

--- Protocol0Midi.py
+++ Protocol0Midi.py
@@ -5,7 +5,7 @@ from typing import Any, Tuple
 
 from _Framework.ControlSurface import ControlSurface, get_control_surfaces
 from _Framework.Util import find_if
-from protocol0 import Protocol0
+from protocol0.application.Protocol0 import Protocol0
 from protocol0.domain.enums.LogLevelEnum import LogLevelEnum
 from protocol0.infra.log import log_ableton
 
