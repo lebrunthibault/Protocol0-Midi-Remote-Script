ef991e304811acc717c86d1c4f745c2e729d2f83
Thibault <thibaultlebrun@live.fr>
2022-02-08 18:43:00 +0100

    'backup'

--- Protocol0Midi.py
+++ Protocol0Midi.py
@@ -5,11 +5,11 @@ from typing import Any, Tuple
 from _Framework.ControlSurface import ControlSurface, get_control_surfaces
 from _Framework.Util import find_if
 from protocol0.application.Protocol0 import Protocol0
-from protocol0.shared.LogLevelEnum import LogLevelEnum
+from protocol0.infra.logging.LoggerService import LoggerService
+from protocol0.shared.logging.LogLevelEnum import LogLevelEnum
 from protocol0.domain.shared.DomainEventBus import DomainEventBus
 from protocol0.domain.shared.System import System
-from protocol0.domain.shared.midi.MidiBytesReceivedEvent import MidiBytesReceivedEvent
-from protocol0.infra.log import log_ableton
+from protocol0.infra.midi.MidiBytesReceivedEvent import MidiBytesReceivedEvent
 
 
 class Protocol0Midi(ControlSurface):
@@ -33,10 +33,11 @@ class Protocol0Midi(ControlSurface):
         self._c_instance.log_message = MethodType(lambda s, message: None, self._c_instance)  # noqa
         self.main_p0_script = find_if(lambda s: isinstance(s, Protocol0), get_control_surfaces())  # type: Protocol0
 
+        self._logger = LoggerService()
         if self.main_p0_script is None:
-            log_ableton("Error: couldn't find main Protocol0 script", level=LogLevelEnum.ERROR)
+            self._logger.log("Error: couldn't find main Protocol0 script", level=LogLevelEnum.ERROR)
             return
-        log_ableton("Notifying Protocol0Midi up", level=LogLevelEnum.DEBUG)
+        self._logger.log("Notifying Protocol0Midi up", level=LogLevelEnum.DEBUG)
         System.client().notify_protocol0_midi_up()
 
     def receive_midi(self, midi_bytes):
@@ -44,4 +45,4 @@ class Protocol0Midi(ControlSurface):
         if self.main_p0_script:
             DomainEventBus.notify(MidiBytesReceivedEvent(midi_bytes))
         else:
-            log_ableton("Received midi input but no Protocol0 script to forward it to", level=LogLevelEnum.ERROR)
+            self._logger.log("Received midi input but no Protocol0 script to forward it to", level=LogLevelEnum.ERROR)
