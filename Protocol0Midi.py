5000554bd12cc51b8e93af788949112dcde1ce5b
Thibault <thibaultlebrun@live.fr>
2022-02-08 14:16:54 +0100

    'backup'

--- Protocol0Midi.py
+++ Protocol0Midi.py
@@ -1,12 +1,14 @@
 from types import MethodType
 
-from p0_system_client import P0SystemClient
 from typing import Any, Tuple
 
 from _Framework.ControlSurface import ControlSurface, get_control_surfaces
 from _Framework.Util import find_if
 from protocol0.application.Protocol0 import Protocol0
 from protocol0.domain.enums.LogLevelEnum import LogLevelEnum
+from protocol0.domain.shared.DomainEventBus import DomainEventBus
+from protocol0.domain.shared.System import System
+from protocol0.domain.shared.midi.MidiBytesReceivedEvent import MidiBytesReceivedEvent
 from protocol0.infra.log import log_ableton
 
 
@@ -35,11 +37,11 @@ class Protocol0Midi(ControlSurface):
             log_ableton("Error: couldn't find main Protocol0 script", level=LogLevelEnum.ERROR)
             return
         log_ableton("Notifying Protocol0Midi up", level=LogLevelEnum.DEBUG)
-        P0SystemClient().notify_protocol0_midi_up()
+        System.client().notify_protocol0_midi_up()
 
     def receive_midi(self, midi_bytes):
         # type: (Tuple) -> None
         if self.main_p0_script:
-            self.main_p0_script.CONTAINER.midi_manager.receive_midi(midi_bytes=midi_bytes)
+            DomainEventBus.notify(MidiBytesReceivedEvent(midi_bytes))
         else:
             log_ableton("Received midi input but no Protocol0 script to forward it to", level=LogLevelEnum.ERROR)
