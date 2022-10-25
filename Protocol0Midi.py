5dc60796fef6e1ad0eb0a90f27e475da398acf8d
Thibault <thibaultlebrun@live.fr>
2022-10-25 18:11:52 +0200

    'backup'

--- Protocol0Midi.py
+++ Protocol0Midi.py
@@ -8,6 +8,7 @@ from protocol0.application.Protocol0 import Protocol0
 from protocol0.domain.shared.event.DomainEventBus import DomainEventBus
 from protocol0.infra.logging.LoggerService import LoggerService
 from protocol0.infra.midi.MidiBytesReceivedEvent import MidiBytesReceivedEvent
+from protocol0.infra.midi.MidiBytesSentEvent import MidiBytesSentEvent
 from protocol0.shared.logging.LogLevelEnum import LogLevelEnum
 
 
@@ -17,7 +18,7 @@ class Protocol0Midi(ControlSurface):
     So we send input to a loopback port connected to this small script and route it back to the main script.
     Alternate solution is more cumbersome: Merge midi ports with something like Bome
 
-    This expects the Protocol0 lint to be in the Remote scripts (will not even appear in the list otherwise)
+    This expects the Protocol0 script to be in the Remote scripts (will not even appear in the list otherwise)
     And will log an error if the Protocol0 script is not used as a Control Surface
     """
 
@@ -33,10 +34,18 @@ class Protocol0Midi(ControlSurface):
         self.main_p0_script = find_if(lambda s: isinstance(s, Protocol0), get_control_surfaces())  # type: Protocol0
 
         self._logger = LoggerService()
+
         if self.main_p0_script is None:
             self._logger.log("Error: couldn't find main Protocol0 script", level=LogLevelEnum.ERROR)
             return
 
+        DomainEventBus.subscribe(MidiBytesSentEvent, self._on_midi_bytes_sent_event)
+
     def receive_midi(self, midi_bytes):
         # type: (Tuple) -> None
         DomainEventBus.emit(MidiBytesReceivedEvent(midi_bytes))
+
+    def _on_midi_bytes_sent_event(self, event):
+        # type: (MidiBytesSentEvent) -> None
+        """Forward midi data to optional midi output ports (e.g. program change for Minitaur)"""
+        self._send_midi(event.midi_bytes)
